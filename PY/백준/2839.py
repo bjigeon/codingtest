b00114c065ac58ce3e148007f4e330df3206dc09
Park Ji Gun <bjigeon@gmail.com>
2022-03-14 15:09:07 +0900

    만들기 & 수정

--- PY/백준/2839.py
+++ PY/백준/2839.py
@@ -1,10 +1,31 @@
 k = int(input())
-cnt = 0 
-if((k//5) % 3 == ):
+cnt = 0
+if(k%5 == 0):
+    print(k//5)
+    print(1)
+    
 
-elif((k%5) % 3 != 0):
+elif(k%5 != 0 and k%3 != 0):
     print(-1)
-elif(k//5 ):
-    cnt += k//5
-    cnt += (k%5)//3
-    print(cnt)
+    print(3)
+
+elif(k%3 == 0):
+    print(k//3)
+    print(4)
+
+# else((k%5) // 3 + k//5 < k//3):
+else:
+    print((k%5) // 3 + k//5)
+    print(2)
+
+
+k = int(input())
+cnt = 0
+if(k%5 == 0):
+    print(k//5)
+elif(k%5 != 0 and k%3 != 0):
+    print(-1)
+elif(k%3 == 0):
+    print(k//3)
+else:
+    print((k%5) // 3 + k//5)
