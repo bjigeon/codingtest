29e3626218714fc2217430dbcb5c8f9813864315
Park Ji Gun <bjigeon@gmail.com>
2022-03-14 16:39:16 +0900

    Update 2839.py

--- PY/백준/2839.py
+++ PY/백준/2839.py
@@ -1,31 +1,47 @@
-k = int(input())
-cnt = 0
-if(k%5 == 0):
-    print(k//5)
-    print(1)
+# k = int(input())
+# cnt = 0
+# if(k%5 == 0):
+#     print(k//5)
+#     print(1)
     
 
-elif(k%5 != 0 and k%3 != 0):
-    print(-1)
-    print(3)
+# elif(k%5 != 0 and k%3 != 0):
+#     print(-1)
+#     print(3)
 
-elif(k%3 == 0):
-    print(k//3)
-    print(4)
+# elif(k%3 == 0):
+#     print(k//3)
+#     print(4)
 
-# else((k%5) // 3 + k//5 < k//3):
-else:
-    print((k%5) // 3 + k//5)
-    print(2)
+# # else((k%5) // 3 + k//5 < k//3):
+# else:
+#     print((k%5) // 3 + k//5)
+#     print(2)
 
 
-k = int(input())
-cnt = 0
-if(k%5 == 0):
-    print(k//5)
-elif(k%5 != 0 and k%3 != 0):
-    print(-1)
-elif(k%3 == 0):
-    print(k//3)
-else:
-    print((k%5) // 3 + k//5)
+# k = int(input())
+# cnt = 0
+# if(k%5 == 0):
+#     print(k//5)
+# elif(k%5 != 0 and k%3 != 0):
+#     print(-1)
+# elif(k%3 == 0):
+#     print(k//3)
+# else:
+#     print((k%5) // 3 + k//5)
+
+
+1 -1
+2 -1
+3 -1
+4 -1
+5 1
+6 2
+7 -1
+8 2
+9 3
+10 2
+11 -1
+12 4
+13 3
+14 -1
