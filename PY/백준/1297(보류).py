f5231dbde0421c486d024b90c8e9533a0c901a59
Park Ji Gun <bjigeon@gmail.com>
2022-04-03 23:37:48 +0900

    수정 & 추가

--- PY/백준/1297(보류).py
+++ PY/백준/1297(보류).py
@@ -1,3 +1,4 @@
+from cmath import sqrt
 import math
 
 # print(52*52)
@@ -6,25 +7,42 @@ import math
 # print()
 # print(math.sqrt((45**2)+(25**2)))
 
-d,s,g=map(int,input().split())
-d2=d**2
-s2=s**2
-g2=g**2
+# d,s,g=map(int,input().split())
+# d2=d**2
+# s2=s**2
+# g2=g**2
 
-print(52**2)
-print(9**2)
-print(16**2)
+# print(52**2)
+# print(9**2)
+# print(16**2)
 
-s1=s
-g1=g
-cnt=0
-cnt1=0
+# s1=s
+# g1=g
+# cnt=0
+# cnt1=0
 
-while((s2 + g2) == d2):
-    s2+=s1
-    cnt+=1
+# while((s2 + g2) == d2):
+#     s2+=s1
+#     cnt+=1
 
-    g2+=g1
-    cnt1+=1
+#     g2+=g1
+#     cnt1+=1
 
-print(cnt,cnt1)
+# print(cnt,cnt1)
+
+# d,s,g = map(int,input().split())
+# d2=d**2
+# s2=s**2
+# g2=g**2
+
+# print(d2)
+# print(s2)
+# print(g2)
+
+# 52**2 = (9+x)**2 + (16+y)**2
+# y = x - 27
+# x2 - 2x - 1251
+
+d,s,g = map(int,input().split())
+print((d**2)*(s/(s+g)**2))
+print((d**2)*(g/(s+g)**2))
---
+++ test.js
@@ -0,0 +1,36 @@
+let a = 52;
+let b = 9;
+let c = 16;
+
+// a * a = (width * width) + (height * height);
+// width : height = 9 : 16
+// 16width = 9height
+// 16 * (a * a) = 9 (height * hieght) + 16 (height + height)
+// 52 = Math.sqrt((16 * (a*a)) - (9*height*height) + (16*height*height));
+// 3height + 4height
+
+// 25 height ^ 2 = 16 * a ^2
+// if(52*52 == 9*9 + 16 * 16){
+//     console.log('sdf');
+// }
+// let height = Math.sqrt((16 * (52*52))) / 25;
+// // let height = (4*a - 52) * -1;
+
+// console.log(height);
+
+// let width = height * (9/16);
+
+// console.log(width);
+
+// console.log(height * width);
+
+// console.log(Math.sqrt(52*52 * (9/16)));
+
+// width*width = 33.28 * 
+// 33.28 , 18.72
+// let Theight = Math.sqrt(33.28 * 18.72);
+// console.log(Theight);
+
+// let width= Math.sqrt(52 * 33.28);
+
+// console.log(width);
