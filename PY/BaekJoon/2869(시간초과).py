3d7c937db41b06cb78dea8930b9b079ee41af4ec
Park Ji Gun <bjigeon@gmail.com>
2022-04-12 13:19:38 +0900

    파일명변경

--- PY/BaekJoon/2869(시간초과).py
+++ PY/BaekJoon/2869(시간초과).py
@@ -20,10 +20,24 @@
 # else:
 #     print(v-(a-b))
 
-import math
+# import math
+# import sys
+# a,b,v = map(int,sys.stdin.readline().split())
+# if a-b == 1:
+#     print(v-b)
+# else:
+    # print(math.ceil(v/(a-b)))
+    # print(round(v/(a-b)))
+
+
+
+# (v-a) 에 (a-b) 로 며칠이 걸려야 도달할 수 있을까? 
+# 2 1 5
+
+# 5-2 1-2
+# 3에 1로
+
+
 import sys
+import math
 a,b,v = map(int,sys.stdin.readline().split())
-if a-b == 1:
-    print(v-b)
-else:
-    print(math.ceil(v/(a-b)))
